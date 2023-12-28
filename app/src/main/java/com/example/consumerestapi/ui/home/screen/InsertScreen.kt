c4fb3ab6121ff7d7162c9523847d9b8cef842d66
franconero14 <nerobalor@gmail.com>
2023-12-28 20:19:09 +0700

    Add OutlinedTextField for "Email" and "Telepon" inputs in Column

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -3,11 +3,15 @@ package com.example.consumerestapi.ui.home.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
 
@@ -32,6 +36,30 @@ fun FormInputSiswa(
             singleLine = true,
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
+        )
+
+        OutlinedTextField(
+            value = insertUiEvent.alamat,
+            onValueChange ={onValueChange(insertUiEvent.copy(alamat = it))},
+            label = { Text("Email") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
+
+        OutlinedTextField(
+            value = insertUiEvent.nohp,
+            onValueChange ={onValueChange(insertUiEvent.copy(nohp = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text("Telepon") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
+
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(bottom = 12.dp)
         )
     }
 }
