a7a0eff301014b669154fb6797e1df7c1b1c3b90
franconero14 <nerobalor@gmail.com>
2023-12-28 20:16:42 +0700

    Add OutlinedTextField for "Name" input in Column

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,12 +1,37 @@
 package com.example.consumerestapi.ui.home.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun FormInputSiswa(
     insertUiEvent: InsertUiEvent,
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
-){}
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange ={onValueChange(insertUiEvent.copy(nama = it))},
+            label = { Text("Nama") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled
+        )
+    }
+}
