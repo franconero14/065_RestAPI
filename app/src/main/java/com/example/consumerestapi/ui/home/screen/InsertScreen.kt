e8e9704c36069f8ac9394314b52ad864e30e7e39
franconero14 <nerobalor@gmail.com>
2023-12-28 21:00:15 +0700

    Defining the Contact Entry Body using a Column containing the Student Input Form and Button to manage input and save contact data

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,7 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
-
+import com.example.consumerestapi.ui.home.viewmodel.InsertUiState
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -37,7 +39,6 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
         )
-
         OutlinedTextField(
             value = insertUiEvent.alamat,
             onValueChange ={onValueChange(insertUiEvent.copy(alamat = it))},
@@ -46,7 +47,6 @@ fun FormInputSiswa(
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled
         )
-
         OutlinedTextField(
             value = insertUiEvent.nohp,
             onValueChange ={onValueChange(insertUiEvent.copy(nohp = it))},
@@ -63,10 +63,36 @@ fun FormInputSiswa(
                 modifier = Modifier.padding(start = 12.dp)
             )
         }
-
         Divider(
             thickness = 8.dp,
             modifier = Modifier.padding(bottom = 12.dp)
         )
+    }
+}
+
+
+@Composable
+fun EntryKontakBody(
+    insertUiState: InsertUiState,
+    onSiswaValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInputSiswa(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onSiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
     }
 }
