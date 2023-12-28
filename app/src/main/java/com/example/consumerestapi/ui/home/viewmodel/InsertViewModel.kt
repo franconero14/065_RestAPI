5cf1458b65f8c563d56c54a862a82ac2f00364d1
franconero14 <nerobalor@gmail.com>
2023-12-28 12:47:33 +0700

    Add InsertUiEvent data class

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
@@ -4,3 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.example.consumerestapi.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){}
+
+data class InsertUiEvent(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val nohp: String = ""
+)
