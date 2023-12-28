0ffdd5470f0cc0faa42f01d8be7c64df1aa1ade1
franconero14 <nerobalor@gmail.com>
2023-12-28 20:21:23 +0700

    Add the KontakRepository interface with the suspend insertKontak function

--- app/src/main/java/com/example/consumerestapi/repository/KontakRepository.kt
+++ app/src/main/java/com/example/consumerestapi/repository/KontakRepository.kt
@@ -5,10 +5,16 @@ import com.example.consumerestapi.network.KontakService
 
 interface KontakRepository {
     suspend fun getKontak(): List<Kontak>
+    suspend fun insertKontak(kontak: Kontak)
 }
 
 class NetworkKontakRepository(
     private val kontakApiService: KontakService
 ) : KontakRepository {
-    override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun getKontak(): List<Kontak> =
+        kontakApiService.getKontak()
+
+    override suspend fun insertKontak(kontak: Kontak) {
+        kontakApiService.insertKontak(kontak)
+    }
 }
