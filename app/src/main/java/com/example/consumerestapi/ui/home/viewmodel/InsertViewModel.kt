2833594fe84f04d8082fa48e01e7c9a64b297aee
franconero14 <nerobalor@gmail.com>
2023-12-28 12:46:10 +0700

    Create an InsertViewModel class with a KontakRepository dependency that extends the ViewModel

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/InsertViewModel.kt
@@ -1,40 +1,6 @@
 package com.example.consumerestapi.ui.home.viewmodel
 
-import android.provider.ContactsContract.CommonDataKinds.Email
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
-import com.example.consumerestapi.model.Kontak
 import com.example.consumerestapi.repository.KontakRepository
 
-class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
-    var insertKontakState by mutableStateOf(InsertUiState)
-}
-
-data class InsertUiEvent(
-    val id: Int = 0,
-    val nama: String = "",
-    val alamat: String = "",
-    val nohp: String = ""
-)
-
-fun InsertUiEvent.toKontak() : Kontak = Kontak(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    nohp = nohp,
-)
-
-data class InsertUiState(
-    val insertUiEvent: InsertUiEvent = InsertUiEvent()
-)
-
-fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
-    id = id,
-    nama = nama,
-    alamat = alamat,
-    nohp = nohp
-)
-
-fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
-    insertUiEvent = toInsertUiEvent()
-)
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel(){}
