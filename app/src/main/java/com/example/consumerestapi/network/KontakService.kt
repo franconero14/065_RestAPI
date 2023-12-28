b99d77865468411ae61d6262cd618250d5dc8287
franconero14 <nerobalor@gmail.com>
2023-12-28 12:41:36 +0700

    Add function to update contacts with PUT method on endpoint 'Kontak/{id}'

--- app/src/main/java/com/example/consumerestapi/network/KontakService.kt
+++ app/src/main/java/com/example/consumerestapi/network/KontakService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.PUT
 import retrofit2.http.Path
 
 interface KontakService {
@@ -19,4 +20,8 @@ interface KontakService {
 
     @POST("kontak")
     suspend fun insertKontak(@Body kontak: Kontak)
+
+    @PUT("kontak/{id}")
+    suspend fun updateKontak(@Path("id") id: Int, @Body kontak: Kontak)
+
 }
