46fbbccb1c5ce9ed8d216587c83a1542b5603d7d
franconero14 <nerobalor@gmail.com>
2023-12-28 20:53:07 +0700

    Add initializer for InsertViewModel in ProviderViewModel.Factory

--- app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/viewmodel/PenyediaViewModel.kt
@@ -5,20 +5,22 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.consumerestapi.KontakAplication
+import com.example.consumerestapi.ui.home.viewmodel.InsertViewModel
 import com.example.consumerestapi.ui.home.viewmodel.HomeViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
 
-//        initializer {
-//            MarsViewModel(apkikasiMars().container.marsPhotosRepository)
-//        }
+
         initializer {
-            HomeViewModel(aplikasiMars().container.kontakRepository)
+            HomeViewModel(aplikasiKontak().container.kontakRepository)
+        }
+        initializer {
+            InsertViewModel(aplikasiKontak().container.kontakRepository)
         }
 
     }
 }
 
-fun CreationExtras.aplikasiMars(): KontakAplication =
+fun CreationExtras.aplikasiKontak(): KontakAplication =
     (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]as KontakAplication)
