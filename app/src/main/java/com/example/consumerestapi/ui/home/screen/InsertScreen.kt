a81c5a76af38b5801ddbd6adcdec03420ad6047c
franconero14 <nerobalor@gmail.com>
2023-12-28 20:13:30 +0700

    Add FormInputStudent fun with InsertUiEvent parameter in InsertScreen

--- app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
+++ app/src/main/java/com/example/consumerestapi/ui/home/screen/InsertScreen.kt
@@ -1,2 +1,12 @@
 package com.example.consumerestapi.ui.home.screen
 
+import androidx.compose.ui.Modifier
+import com.example.consumerestapi.ui.home.viewmodel.InsertUiEvent
+
+
+fun FormInputSiswa(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){}
